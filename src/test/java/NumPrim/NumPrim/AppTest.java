a49613af5c803d34936ae35b6217b916aca11656
micmus94 <musico.michele@libero.it>
2020-10-28 13:17:33 +0100

    Add files via upload
    test uploaded

--- src/test/java/NumPrim/NumPrim/AppTest.java
+++ src/test/java/NumPrim/NumPrim/AppTest.java
@@ -1,38 +1,52 @@
 package NumPrim.NumPrim;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.*;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Arrays;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+public class AppTest {
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertEquals(5,myMath.prime(20) );
-    }
+	private static MyMath math;
+	@BeforeClass
+	public static void prepare() throws Exception {
+		math=new MyMath();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void prepare() throws Exception {
+		math=new MyMath();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void primeGeneratorWorks() {
+		assertEquals(new LinkedList<Integer>(), math.primeGenerator(1));
+		assertEquals(Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19),math.primeGenerator(20));
+	}
+	
+	@Test
+	public void primeWorks() {
+		assertFalse(math.prime(9));
+		assertFalse(math.prime(1));
+		assertTrue(math.prime(2));
+		List<Integer>primes=new Arrays.asList(11,13,17,23,31,41,53,67,83,101);
+		for(int prime:primes) {
+			assertTrue(math.prime(prime));
+		}
+	}
+
 }
